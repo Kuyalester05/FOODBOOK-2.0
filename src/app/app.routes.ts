aff5d40d353dd1b3744cecd1487e14df17fc7b9c
websysbylesz <mrleszterrrboleche@gmail.com>
2025-12-11 22:02:42 +0800

    edit post

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Landing } from './components/landing/landing';
 import { Home } from './components/home/home';
+import { Post } from './components/post/post';
 
 export const routes: Routes = [
     { path: '', component: Landing },
     { path: 'home', component: Home },
+    { path: 'post', component: Post },
 ];
---
+++ src/app/components/post/post.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Navbar } from "../navbar/navbar";
+
+@Component({
+  selector: 'app-post',
+  imports: [Navbar],
+  templateUrl: './post.html',
+  styleUrl: './post.css',
+})
+export class Post {
+
+}
